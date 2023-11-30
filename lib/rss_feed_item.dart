8112990c61a432806ae254507fa07af47f9c323c
Sergiy Vergun <sergiyvergun01@gmail.com>
2023-11-30 21:09:45 +0100

    Implement ios

---
+++ lib/rss_feed_item.dart
@@ -0,0 +1,46 @@
+class RssFeedItem {
+  final String? guid;
+  final String? title;
+  final String? description;
+  final String? link;
+  final String? author;
+  final String? pubDate;
+
+  final String? enclosure;
+
+  RssFeedItem(
+      {required this.guid,
+      required this.title,
+      required this.description,
+      required this.link,
+      required this.author,
+      required this.pubDate,
+      required this.enclosure});
+
+  factory RssFeedItem.fromIOSJson(Map<String, dynamic> json) {
+    return RssFeedItem(
+        title: json['title'],
+        description: json['description'],
+        link: json['link'],
+        author: json['author'],
+        pubDate: json['pubDate'],
+        enclosure: json['enclosure'],
+        guid: json['guid']);
+  }
+
+  factory RssFeedItem.fromAndroidJson(Map<String, dynamic> json) {
+    return RssFeedItem(
+        title: json['title'],
+        description: json['description'],
+        link: json['link'],
+        author: json['author'],
+        pubDate: json['pubDate'],
+        enclosure: json['image'],
+        guid: json['guid']);
+  }
+
+  @override
+  String toString() {
+    return 'RssFeedItem{guid: $guid, title: $title, description: $description, link: $link, author: $author, pubDate: $pubDate, enclosure: $enclosure}';
+  }
+}
